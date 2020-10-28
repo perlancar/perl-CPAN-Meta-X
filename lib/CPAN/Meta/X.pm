fcbb84553957774bebb12031eb07695f76d50906
perlancar (on pc-home) <perlancar@gmail.com>
2020-10-29 06:06:33 +0700

    Initial commit

---
+++ lib/CPAN/Meta/X.pm
@@ -0,0 +1,167 @@
+package CPAN::Meta::X;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: List of custom (x_*) keys in CPAN distribution metadata being used in the wild
+
+=head1 DESCRIPTION
+
+L<The CPAN distribution metadata specification|CPAN::Meta::Spec> allows custom
+keys (those that begin with C<x_> or C<X_> to be added to the metadata. Over
+time, some keys are being used for their specific purposes. This document tries
+to list them.
+
+This document also lists custom phases and relationships in the
+L<prereqs|CPAN::Meta::Spec/PREREQUISITES> hash that are being used.
+
+
+=head1 LIST OF CUSTOM DISTRIBUTION METADATA KEYS
+
+=head2 x_contributors
+
+List of contributors in a release.
+
+Examples:
+
+References:
+
+=over
+
+=item * DAGOLDEN, L<https://perlmaven.com/how-to-add-list-of-contributors-to-the-cpan-meta-files>
+
+=item * SZABGAB, L<https://github.com/book/CPANio/issues/7>
+
+=back
+
+
+=head2 x_deprecated
+
+
+=head2 x_provides_scripts
+
+List what scripts are being provided in the distribution. The structure is
+modeled after the standard L<provides|CPAN::Meta::Spec/provides> hash.
+
+Examples:
+
+So say your distribution provides a "csv2json" script, your F<META.json> would
+contain:
+
+ "x_provides_scripts": {
+   "csv2json": {
+     "version": "0.1",
+     "file": "bin/csv2json"
+   }
+ }
+
+References:
+
+=over
+
+=item * TOBYINK, L<https://perlmonks.org/?node_id=11123240>
+
+=back
+
+
+=head1 LIST OF CUSTOM PREREQS PHASES
+
+=head2 x_benchmarks phase
+
+Express that the current distribution is benchmarking the specified module.
+
+References:
+
+=over
+
+=item * PERLANCAR, L<https://perlancar.wordpress.com/2016/12/28/x_-prereqs/>
+
+=back
+
+
+=head2 x_Dist_Zilla
+
+A big structure recording information related to L<Dist::Zilla> which presumably
+is used to build the current distribution. Some of the things being put in here
+include: perl version used to build the distribution, Dist::Zilla plugins used
+to build the distribution, and so on.
+
+
+=head2 x_authority
+
+
+=head2 x_examples
+
+List prerequisites for example scripts.
+
+References:
+
+=over
+
+=item * KENTNL, L<https://perlancar.wordpress.com/2016/12/28/x_-prereqs/>
+
+=back
+
+
+=head2 x_generated_by_perl
+
+
+=head2 x_mentions phase
+
+Express that the current distribution is mentioning the specified module.
+
+References:
+
+=over
+
+=item * PERLANCAR, L<https://perlancar.wordpress.com/2016/12/28/x_-prereqs/>
+
+=back
+
+
+=head2 x_spdx_expression
+
+
+=head2 x_serialization_backend
+
+
+=head2 x_spec phase
+
+Express that the current distribution is following a specification defined in
+the specified module.
+
+References:
+
+=over
+
+=item * PERLANCAR, L<https://perlancar.wordpress.com/2016/12/28/x_-prereqs/>
+
+=back
+
+
+=head2 x_static_install
+
+
+=head1 LIST OF CUSTOM PREREQS RELATIONSHIPS
+
+=head1 x_benchmarks relationship
+
+See L</"x_benchmarks phase">.
+
+
+=head1 x_mentions relationship
+
+See L</"x_mentions phase">.
+
+
+=head1 x_spec relationship
+
+See L</"x_spec phase">.
+
+
+=head1 SEE ALSO
+
+L<CPAN::Meta::Spec> - Specification for CPAN distribution metadata
